997459b0b331241d258a2249f8c4f1a006b26168
Bingran Hu <44424231+Bill-hbrhbr@users.noreply.github.com>
2019-05-18 04:09:55 -0400

    Update 0214_Shortest_Palindrome.cpp

--- Cpp/0214_Shortest_Palindrome.cpp
+++ Cpp/0214_Shortest_Palindrome.cpp
@@ -1,3 +1,9 @@
+/*
+ * Equally weighted hash algorithm
+ * O(n) beats 99.21% C++ solutions
+ */
+#include <algorithm> //Reverse string objects
+using namespace std;
 class Solution {
 public:
     bool check(const string &s, int endIndex) {
@@ -12,25 +18,22 @@ public:
         return true;
     }
     string shortestPalindrome(string s) {
-        const int PRIME = 999983;
+        const int PRIME = 999983; //Large Prime
         int length = s.size();
         string returnStr(s);
+        reverse(returnStr.begin(), returnStr.end());
         if (length < 2) {
             return returnStr;
         }
-        int centerIndex = 0, fastIndex = 0, hash1 = 0, hash2 = 0;
-        while (fastIndex < length) {
-            hash2 = (hash2 + s[fastIndex]) % PRIME;
-            if (fastIndex % 2 == 1) {
-                hash2 = (hash2 + PRIME - s[centerIndex]) % PRIME;
-                ++centerIndex;
-            } else {
-                hash1 = (hash1 + s[centerIndex]) % PRIME;
-            }
-            ++fastIndex;
+        int hash1 = 0, hash2 = 0;
+        for (int i = 0; i < (length + 1) / 2; ++i) {
+            hash1 = (hash1 + s[i]) % PRIME;
         }
-        fastIndex = length - 1;
-        centerIndex = length / 2;
+        for (int i = length / 2; i < length; ++i) {
+            hash2 = (hash2 + s[i]) % PRIME;
+        }
+        int fastIndex = length - 1;
+        int centerIndex = length / 2;
         while (fastIndex >= 0) {
             if (hash1 == hash2 && check(s, fastIndex)) {
                 break;
@@ -46,7 +49,7 @@ public:
         }
         
         for (int i = fastIndex + 1; i < length; ++i) {
-            returnStr = s[i] + returnStr;
+            returnStr += s[i]; //Efficient concatenation
         }
         return returnStr;
     }
